a8ec4dcb66c5a674829ae4a14e416d5bd6d6d606
Lorenzo Breedveld <2105144@talnet.nl>
2020-09-01 10:09:16 +0200

    taak 1 af

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+  $medewerkers = [
+    [
+      'voornaam' => 'Willem',
+      'achternaam' => 'van Oranje',
+      'functie'  => 'koning'
+    ],
+    [
+      'voornaam' => 'Donald',
+      'achternaam' => 'Trump',
+      'functie'  => 'president'
+    ]
+  ];
+  echo $medewerkers[0]['voornaam'];
+  echo $medewerkers[0]['achternaam'];
+  echo "is een" ;
+   echo $medewerkers[0]['functie']; 
+  echo "<br>";
+  echo $medewerkers[1]['voornaam'];
+  echo $medewerkers[1]['achternaam'];
+  echo "is een" ;
+   echo $medewerkers[1]['functie']; 
 
+  
+  ?>
+    
+  </body>
