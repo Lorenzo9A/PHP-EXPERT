eecd2a34b3ac641748af936106eaf74599609a9d
Lorenzo Breedveld <2105144@talnet.nl>
2020-09-01 10:52:05 +0200

    taak 2

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$medewerkers = [
+  [
+    'voornaam' => 'Willem',
+    'achternaam' => 'van Oranje',
+    'functie'  => 'koning'
+  ],
+  [
+    'voornaam' => 'Donald',
+    'achternaam' => 'Trump',
+    'functie'  => 'president'
+  ],
+  [
+    'voornaam' => 'Homer',
+    'achternaam' => 'Simpson',
+    'functie'  => 'fabrieksarbeider'
+  ],
+  [
+    'voornaam' => 'Johan',
+    'achternaam' => 'Cruyff',
+    'functie'  => 'voetbalicoon'
+  ]
+  ,
+  [
+    'voornaam' => 'Badr',
+    'achternaam' => 'Hari',
+    'functie'  => 'Kickboxer'
+  ]
+];
+for($medewerkers = 0; $medewerkers < 5;){
+
+    echo $medewerkers[0]['voornaam'];
+}
+?>
+
