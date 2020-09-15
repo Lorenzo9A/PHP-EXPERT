27432e1aa28dc1d9a799843b27f1f1aee1942e04
Lorenzo Breedveld <2105144@talnet.nl>
2020-09-15 11:17:47 +0200

    taak 4 af

---
+++ 2-OOP/taak04/index.php
@@ -0,0 +1,39 @@
+[11:17] Bruce Amende
+    
+
+<?php
+ 
+classScooter{
+$merk;
+$kleur;
+$inhoudTank;
+}
+
+
+ 
+    $peugeot = newScooter();
+    $peugeot->merk = 'peugeot',;
+    $peugeot->kleur = 'zwart',;
+    $peugeot->inhoudTank = '7',;
+    $peugeot->checkInhoud();
+ 
+    $piago = newScooter();
+    $piago->merk = 'piago',;
+    $piago->kleur = 'rood',;
+    $piago->inhoudTank = '6',;
+    $piago->checkInhoud();
+
+functioncheckInhoud(){ 
+if($this->inhoudTank == 0){
+echo'De tank is leeg!'
+            }
+else{
+echo'de tank is niet leeg!';
+        }
+    }
+
+?>
+
+
+
+
