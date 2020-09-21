40ea8b9fb9258f2edccbf4c109dde01b2ebbfbfb
Lorenzo Breedveld <2105144@talnet.nl>
2020-09-21 19:17:54 +0200

    taak 6

---
+++ 2-OOP/taak06/index.php
@@ -0,0 +1,22 @@
+<?php
+ 
+class Robot{
+$naam;
+$geluid;
+$voortstuwing;
+
+}
+
+    $Wally = newRobot();
+    $Wally->naam = 'wal-e',;
+    $Wally->geluid = 'beepboop',;
+    $Wally->voortstuwing = "rollend",;
+ 
+    $Wolly = newRobot();
+    $Wolly->naam = 'wol-e',;
+    $Wolly->geluid = 'boopbeep',;
+    $Wolly->voortstuwing = "lopend",;
+    functiongetSound(){ 
+        (){  }
+
+?>
