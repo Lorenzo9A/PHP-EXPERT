0152df7be0636af538d346f6d0b2ce76f4c4b47d
Lorenzo Breedveld <2105144@talnet.nl>
2020-10-06 14:25:13 +0200

    taak 2 af

--- 3-PDO-SELECT/taak02/index.php
+++ 3-PDO-SELECT/taak02/index.php
@@ -6,13 +6,8 @@ $user = 'root';
 $pass = '';
  
 $dsn = "mysql:host=$host;dbname=$db;";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
+     $pdo = new PDO($dsn, $user, $pass);
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
--- 3-PDO-SELECT/taak03/index.php
+++ 3-PDO-SELECT/taak03/index.php
@@ -1,9 +1,28 @@
+
 <?php
-
-$sql = 'SELECT * FROM users WHERE email = :email AND status= :status';
-$database_connectie->prepare($sql);
-$statement->bindParam(":email", $email);
-$statement->bindParam(":status", $status);
-$user = $statement->fetch();
-
+ 
+$host = 'localhost';
+$db   = 'toolsforever';
+$user = 'root';
+$pass = '';
+$database_connectie = new PDO("mysql:host=$database_lokatie;dbname=$database_naam", $database_gebruiker, $database_wachtwoord);
+ 
+$dsn = "mysql:host=$host;dbname=$db;";
+try {
+     $pdo = new PDO($dsn, $user, $pass);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+ 
+$sql = 'SELECT * FROM users WHERE firstname = :Mohammed';
+$stmt = $pdo->prepare('SELECT * FROM users WHERE firstname = :Mohamed');
+$stmt->execute(['firstname' => $firstname]);
+$user = $stmt->fetch();
+ 
+foreach ($stmt as $row)
+{
+    echo $row['firstname'] . "\n";
+}
+ 
 ?>
+
