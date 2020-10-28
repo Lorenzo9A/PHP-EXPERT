c4b76333f38a3d4d5ae222e4a97fbb827e799ed1
Lorenzo Breedveld <2105144@talnet.nl>
2020-10-28 14:34:54 +0100

    taak update

---
+++ 6_PDO-UPDATE/taak01/form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>update</title>
+</head>
+<body>
+<form action="update.php" method=""></form>
+<label for="firstname">First Name:</label>
+<input type="text" name="firstname" id="firstname"></p>  
+<input type="submit" value="Submit"></form>
+</body>
+</html>
